3436c6e506912005245c5544f3216edaa3e7db52
James Stamps Jr <jstampshere@gmail.com>
2020-04-16 18:36:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,12 +6,12 @@ def subtraction(num1, num2)
   return num1 - num2
 end
 
-def division(num1, num2)
-return num1 / num2
-end
-
 def multiplication(num1, num2)
   return num1 * num2
+end
+
+def division(num1, num2)
+  return num1 / num2
 end
 
 def modulo(num1, num2)
